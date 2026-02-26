1f7680b617d1f94281778f92753026e59a694b40
Pavel Barysevich <pavelbarysevich@gmail.com>
2026-02-26 17:08:02 +0300

    [Feature] Пользовательские значки для аннотации карты для карты SwiftUI MapKit

---
+++ MapApp/Views/LocationMapAnnotationView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    var body: some View {
+        VStack (spacing: 0) {
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundStyle(.white)
+                .padding(6)
+                .background(.accent)
+                .cornerRadius(36)
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.accent)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom, 40)
+            
+        }
+    }
+}
+
+#Preview {
+    LocationMapAnnotationView()
+}
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -3,23 +3,14 @@ import MapKit
 
 struct LocationsView: View {
     
+    
     @Environment(LocationsViewModel.self) private var vm: LocationsViewModel
     @State private var cameraPosition: MapCameraPosition = .automatic
     
     var body: some View {
         ZStack {
-            Map(position: $cameraPosition)
-                .ignoresSafeArea()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .onAppear {
-                    cameraPosition = .region(vm.mapRegion)
-                }
-                .onChange(of: RegionProxy(vm.mapRegion)) { _, _ in
-                    withAnimation(.easeInOut) {
-                        cameraPosition = .region(vm.mapRegion)
-                    }
-                }
-            
+            mapLayer
+           
             VStack (spacing: 0) {
                 
                 header
@@ -27,21 +18,53 @@ struct LocationsView: View {
                 
                 Spacer()
                 
-                ZStack {
-                    ForEach(vm.locations) { location in
-                        if vm.mapLocation == location {
-                            LocationPreviewView(location: location)
-                                .shadow(color: .black.opacity(0.3), radius: 20)
-                                .padding()
-                                .transition(.asymmetric(
-                                    insertion: .move(edge: .trailing),
-                                    removal: .move(edge: .leading)))
+                locationsPreviewStack
+            }
+        }
+    }
+    
+    private var mapLayer: some View {
+        Map(position: $cameraPosition) {
+            ForEach(vm.locations) { location in
+                Annotation(location.name, coordinate: location.coordinates) {
+                    LocationMapAnnotationView()
+                        .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                        .shadow(radius: 10)
+                        .onTapGesture {
+                            vm.showNextLocation(location: location)
                         }
-                    }
                 }
             }
         }
-        
+        .ignoresSafeArea()
+        .onAppear {
+            let center = vm.mapRegion.center
+            let closeSpan = MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004)
+            let closeRegion = MKCoordinateRegion(center: center, span: closeSpan)
+            cameraPosition = .region(closeRegion)
+        }
+        .onChange(of: RegionProxy(vm.mapRegion)) { _, _ in
+            withAnimation(.easeInOut) {
+                let center = vm.mapRegion.center
+                let closeSpan = MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004)
+                let closeRegion = MKCoordinateRegion(center: center, span: closeSpan)
+                cameraPosition = .region(closeRegion)
+            }
+        }
+    }
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: .black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
 }
 
@@ -74,13 +97,13 @@ extension LocationsView {
                 Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
                     .font(.title2)
                     .fontWeight(.black)
-                    .foregroundStyle(.black)
+                    .foregroundStyle(.wOne)
                     .frame(height: 55)
                     .frame(maxWidth: .infinity)
                     .overlay(alignment: .leading) {
                         Image(systemName: "arrow.down")
                             .font(.headline)
-                            .foregroundStyle(.black)
+                            .foregroundStyle(.wOne)
                             .padding()
                             .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
                     }
@@ -92,8 +115,10 @@ extension LocationsView {
             }
             
         }
-        .background(.thickMaterial)
+        .background(.thinMaterial)
         .cornerRadius(20)
         .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
+
+
